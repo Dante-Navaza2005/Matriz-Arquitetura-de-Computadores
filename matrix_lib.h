31c0ea255da27be678b2b8e833e299c13e735a4b
Maria Laura Soares <mlaura0504bs@gmail.com>
2025-08-29 22:14:10 -0300

    segunda parte e uso do timer

---
+++ matrix_lib.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct matrix Matrix;
+struct matrix {
+    unsigned long int height; 
+    unsigned long int width;  
+    float *rows;              
+};
+
+int scalarMatrixMult(float scalar_value, struct matrix *matrix);
+
+int matrixMatrixMult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC);
