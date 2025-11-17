eb479a481a1bd7e206f1d2e8016497d3e6163927
Dante <dantenavaza2005@gmail.com>
2025-11-17 13:02:01 -0300

    consertando timer.c

--- cuda/timer.c
+++ cuda/timer.c
@@ -1,16 +1,13 @@
-#ifndef TIMER_H
-#define TIMER_H
-
-#include <sys/time.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-float timedifference_msec(struct timeval t0, struct timeval t1);
-
-#ifdef __cplusplus
+#include "timer.h"
+/*
+* float timedifference_msec(struct timeval t0, struct timeval t1)
+*
+* Recebe uma marca de tempo t0 e outra marca de tempo t1 (ambas do
+* tipo struct timeval) e retorna a diferenca de tempo (delta) entre
+* t1 e t0 em milisegundos (tipo float).
+*/
+float timedifference_msec(struct timeval t0, struct timeval t1)
+{
+return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }
-#endif
 
-#endif
